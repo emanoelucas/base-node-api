0891f4a2e4db22a19531c90c60ef5f391f6b7d42
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-17 20:25:42 -0300

    api routes example

---
+++ src/main/routes/index.ts
@@ -0,0 +1,21 @@
+import { Router, Express } from 'express'
+const routes = Router()
+
+export const setupRoutes = (app: Express) => {
+  
+  app.use('/api', [
+      routes.get('/', (req, res, next) => {
+        res.send(
+          {
+            status: 200,
+            message: "OK"
+          }
+        )
+      })
+    ]
+  )
+  
+  app.use('/', (req, res, next) => {
+    res.send('o')
+  })
+}
