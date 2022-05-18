c1b061f1324e8c4d0bbb8afe97aaa86814752663
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-18 09:48:18 -0300

    Revert "custom middleware"
    This reverts commit 84c4d0cdca1b9457fdd85b5a72ad9beba06110a4.

--- src/main/middlewares/index.ts
+++ src/main/middlewares/index.ts
@@ -4,7 +4,6 @@ import bodyParser from 'body-parser'
 
 import cors from "./cors"
 import jsonParser from "./json-parser"
-import customMiddleware from "./custom-middleware";
 
 export const setupMiddlewares = (app: Express) => {
 	app.use(cors)
@@ -12,7 +11,6 @@ export const setupMiddlewares = (app: Express) => {
 	app.use(helmet())
 	app.use(bodyParser.json())
 	app.use(bodyParser.urlencoded({ extended: true }))
-	app.use(customMiddleware)
 }
 
 
