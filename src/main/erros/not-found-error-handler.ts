83d189af45c4463cd73f939f2cd01b1c0cb22d82
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-18 20:59:27 -0300

    refactor on middlewares

--- src/main/erros/not-found-error-handler.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express'
-
-import { NotFoundError } from '../../utils/erros/http'
-
-export default ( request: Request, response: Response, next: NextFunction) => {
-  next(new NotFoundError(`A rota '${request.originalUrl}' não foi encontrada`))
-}
