ab54135990da88dfe1e7442e4b226940b2facfae
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-19 16:57:35 -0300

    refactor on export

--- src/utils/logger/messages/error-log-message.ts
+++ src/utils/logger/messages/error-log-message.ts
@@ -1,3 +1,3 @@
-export = (method: string, url: string, status: number, message: string, startTime: number, host?: string) => {
+export default (method: string, url: string, status: number, message: string, startTime: number, host?: string) => {
   return `"${method}" request to "${host}${url}" failed, response code: "${status}", response message: ${message}, response time: "${Date.now()-startTime}" ms`
 }
--- src/utils/logger/messages/info-log-message.ts
+++ src/utils/logger/messages/info-log-message.ts
@@ -1,3 +1,3 @@
-export = (method: string, url: string, ip: string, host?: string) => {
+export default (method: string, url: string, ip: string, host?: string) => {
   return `"${method}" request to "${host}${url}", client IP address: "${ip}"`
 }
