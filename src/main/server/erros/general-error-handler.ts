3dc0c2ab506d5f37f38b4120d6ffed02312a0058
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-24 13:22:48 -0300

    rearrange http erros

--- src/main/server/erros/general-error-handler.ts
+++ src/main/server/erros/general-error-handler.ts
@@ -1,15 +1,17 @@
 import { Request, Response, NextFunction } from 'express'
 
-import { IHttpError } from '../../../utils/erros/http/IHttpError'
+import { IHttpError } from './../../../utils/http/erros/IHttpError'
+import { IHttpResponse } from './../../../utils/http/response/IHttpResponse'
 
 export default (error: IHttpError, req: Request, res: Response, next: NextFunction) => {
   
   const internalError = 500
-
   res.status(error.status || internalError)
-  res.send({
-    error: error.name,
-    message: error.message
-  })
+  const response: IHttpResponse = {
+    message: error.message,
+    success: false,
+    data: {}
+  }
+  res.send(response)
   error.stack ? console.log('\n', 'stack:', error.stack) : console.log('\n')
 }
--- src/main/server/erros/not-found-error-handler.ts
+++ src/main/server/erros/not-found-error-handler.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 
-import { NotFoundError } from '../../../utils/erros/http'
+import { NotFoundError } from '../../../utils/http/erros'
 
 export default ( req: Request, res: Response, next: NextFunction) => {
   next(new NotFoundError(`route "${req.originalUrl}" was not found`))
