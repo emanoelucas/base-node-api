477cb07cc92f02472fd645f0d2c55424f065f165
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-26 16:47:47 -0300

    authentication user

---
+++ src/app/cases/users/authentication.ts
@@ -0,0 +1,35 @@
+
+import { loadUserByEmail } from '../../../infra/repositories/users'
+import encrypter from '../../../utils/libraries/encrypter'
+import { NotFoundError, BadRequestError } from "../../../utils/http/erros"
+class Authentication {
+  
+  private loadUserByEmail: typeof loadUserByEmail
+  private encrypter: typeof encrypter
+  constructor () {
+    this.loadUserByEmail = loadUserByEmail
+    this.encrypter = encrypter
+  }
+
+  async auth (email: string, password: string) {
+
+    const includePassword = {
+        attributes: {
+        include: ['password']
+      }
+    }
+
+    const user = await this.loadUserByEmail.load(email, includePassword)
+    if (!user) {
+      throw new NotFoundError('User not found')
+    }
+
+    const validPassword = await this.encrypter.compare(password, user.password)
+    if (!validPassword)
+      throw new BadRequestError('Password incorrect')
+
+    return user.reload()
+  }
+}
+
+export default new Authentication()
