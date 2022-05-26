042b58d50bf795a406bc34ca469a29f283f4de10
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-26 20:28:39 -0300

    refator message

--- src/main/server/erros/not-found-error-handler.ts
+++ src/main/server/erros/not-found-error-handler.ts
@@ -3,5 +3,5 @@ import { Request, Response, NextFunction } from 'express'
 import { NotFoundError } from '../../../utils/http/erros'
 
 export default ( req: Request, res: Response, next: NextFunction) => {
-  next(new NotFoundError(`route "${req.originalUrl}" was not found`))
+  next(new NotFoundError(`route '${req.originalUrl}' was not found`))
 }
