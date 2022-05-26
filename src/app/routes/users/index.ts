c5550cd46f20dd44bb1666a50d276322a242db68
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-26 17:50:30 -0300

    routes get and update

--- src/app/routes/users/index.ts
+++ src/app/routes/users/index.ts
@@ -1,11 +1,15 @@
 import Router from 'express'
 
+import tokenParser from '../../../main/middlewares/token-parser'
+
 import signUpRoute from './signup-route'
 import signInRoute from './singin-route'
+import getRoute from './get-route'
 
 const userRoutes = Router()
 
 userRoutes.post('/signup', signUpRoute)
 userRoutes.post('/signin', signInRoute)
+userRoutes.get('/:id', tokenParser, getRoute)
 
 export default userRoutes
