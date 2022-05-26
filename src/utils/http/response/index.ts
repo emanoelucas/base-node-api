8a8a076e5b79e70f13e5e42e98f75ef5c03dfd1e
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-26 16:16:28 -0300

    refactor on http default response(fail and sucess)

--- src/utils/http/response/index.ts
+++ src/utils/http/response/index.ts
@@ -1,16 +1,33 @@
 import { IHttpDefaultResponse } from "./IHttpDefaultResponse"
 import { InternalServerError } from "../erros"
+import MissingParamError from "../../erros/missing-param-error"
 
 class HttpResponse {
-  build (params: any, success: boolean = true) {
+  
+  fail (params: any) {
+    
+    if (typeof params !== 'object')
+      throw new InternalServerError('Invalid fail response pattern')
 
-    if (!params.message || !params.data)
-      throw new InternalServerError('Invalid response pattern')
+    if (!params.message)
+      throw new MissingParamError('Missing fail response message')
+    
+    const response = {
+      message: params.message,
+      sucess: false
+    }
+    return Object.assign(response, params)
+    
+  }
+
+  sucess (params: any ) {
+    if ( typeof params !== 'object' || !params.message || !params.data )
+      throw new InternalServerError('Invalid sucess response pattern')
 
     const defaultResponse: IHttpDefaultResponse = {
       message: params.message,
       data: params.data,
-      success: success
+      success: true
     }
 
     return Object.assign(defaultResponse, params)
