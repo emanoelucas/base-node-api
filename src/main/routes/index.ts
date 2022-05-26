035a0be6b28b450aa01119f7d249a736d5a3b9d9
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-26 20:29:56 -0300

    removed example route

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -3,16 +3,5 @@ import { Express } from 'express'
 import userRoutes from './../../app/routes/users'
 
 export const setupRoutes = (app: Express) => {
-  
-  app.use('/', (req, res, next) => {
-      res.send(
-        {
-          status: 200,
-          message: "OK"
-        }
-      )
-    }
-  )
-
   app.use('/user', userRoutes)
 }
