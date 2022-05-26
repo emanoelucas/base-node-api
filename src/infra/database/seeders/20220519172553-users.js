25e7987257e628e1792deb0f117f42e3cf265d90
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-26 16:45:48 -0300

    fixed seed

--- src/infra/database/seeders/20220519172553-users.js
+++ src/infra/database/seeders/20220519172553-users.js
@@ -5,8 +5,10 @@ module.exports = {
     return queryInterface.bulkInsert('users', [{
       id: 1,
       name: 'emanoel',
+      last_name: 'costa',
+      phone_number: '+5584999999999',
       email: 'lucasrodrigues52@gmail.com',
-      hashed_password: '1234',
+      password: '1234',
       created_at: new Date(),
       updated_at: new Date()
     }]);
