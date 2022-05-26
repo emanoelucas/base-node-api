9493f81031d118ffffaab91c0f64f76e291fe308
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-26 16:48:00 -0300

    singup route

---
+++ src/app/routes/users/signup-route.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from 'express'
+
+import requestValidator from '../../../utils/validators/request-body-validator'
+import creation from '../../cases/users/creation'
+import HttpResponse from './../../../utils/http/response'
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const requiredParams = ['name', 'lastName', 'phoneNumber', 'email', 'password', 'repeatPassword']
+    const body = req.body
+  
+    requestValidator(requiredParams, body)
+
+    const user = await creation.create(body.name, body.lastName, body.phoneNumber, body.email, body.password, body.repeatPassword)
+
+    res.send(
+      HttpResponse.sucess( { message: 'Account created', data: {user} } )
+    )
+
+  } catch (error) {
+    next(error)
+  }
+}
