00d5f1666cb10a1b6d163c8d3c72321c87772418
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-05-30 20:03:55 -0300

    refactor exports

--- src/infra/repositories/users/index.ts
+++ src/infra/repositories/users/index.ts
@@ -1,9 +1,13 @@
 import loadUserByEmail from "./load-user-by-email"
 import saveUser from "./save-user"
 import loadUserById from "./load-user-by-id"
+import updateUserProfile from "./update-user-profile"
+import setUserParameter from "./set-user-parameter"
 
 export {
   loadUserByEmail,
   saveUser,
-  loadUserById
+  loadUserById,
+  updateUserProfile,
+  setUserParameter
 }
