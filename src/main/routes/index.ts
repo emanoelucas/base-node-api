e083fc0ef8d35af2224012329a3d3369f4b242b1
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-06-02 20:24:34 -0300

    health check

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -1,7 +1,10 @@
-import { Express } from 'express'
+import { Express, Request, Response } from 'express'
 
 import userRoutes from './../../app/routes/users'
 
 export const setupRoutes = (app: Express) => {
   app.use('/user', userRoutes)
+  app.use('/check', (req: Request, res: Response) => {
+    res.status(200).send({message: 'OK'})
+  })
 }
