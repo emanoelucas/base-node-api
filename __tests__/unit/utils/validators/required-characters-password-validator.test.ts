21e7182a97de3cec116a469cbcd4c6221e976325
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-06-03 17:06:47 -0300

    required password validator test

--- __tests__/unit/utils/validators/required-characters-password-validator.test.ts
+++ __tests__/unit/utils/validators/required-characters-password-validator.test.ts
@@ -14,4 +14,8 @@ describe('Required Password validator', () => {
     const password = 'abcdefgh'
     expect(() => { requiredPassword(password) }).toThrow(BadRequestError)
 	})
+  it('should proceed if it has 8 or more characters with at least 1 number and one letter', () => {
+    const password = 'abcdefgh12'
+    expect(requiredPassword(password)).toBeUndefined()
+	})
 })
