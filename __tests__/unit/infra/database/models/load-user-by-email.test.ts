1b6d7a1ad8e4e485de462fa1e6a2d637e4e835ec
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-06-07 15:29:03 -0300

    load user by email test

---
+++ __tests__/unit/infra/database/models/load-user-by-email.test.ts
@@ -0,0 +1,25 @@
+const userMock = require('../../../../../__mocks__/user-model')
+import { loadUserByEmail } from '../../../../../src/infra/repositories/users'
+import MissingParamError from '../../../../../src/utils/erros/missing-param-error'
+
+jest.mock('./../../../../../src/infra/database/models/user', () => {
+  return userMock
+})
+
+describe('Load user by email repository', () => {
+	it('should return a user if is found', async () => {
+    const mockEmail = 'john@mail.com'
+    const user = await loadUserByEmail.load(mockEmail)
+    expect(user?.email).toBe(mockEmail)
+	})
+  it('should return null no user is found', async () => {
+    const mockEmail = 'notFoundEmail@mail.com'
+    const user = await loadUserByEmail.load(mockEmail)
+    expect(user).toBeNull()
+	})
+  it('should throw if no email is provided', async () => {
+    const mockEmail = ''
+    const promise = loadUserByEmail.load(mockEmail)
+    expect(promise).rejects.toThrow(new MissingParamError('email'))
+	})
+})
