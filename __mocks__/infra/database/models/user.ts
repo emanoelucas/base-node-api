3c7a758d58024cec12b141f956a7b2536cc587ef
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-06-07 15:17:36 -0300

    user model mock

---
+++ __mocks__/infra/database/models/user.ts
@@ -0,0 +1,49 @@
+const sequelizeMock = require('sequelize-mock-v5')
+
+const DBConnectionMock = new sequelizeMock()
+
+const userDataMock = [
+  {
+    id: '03c22d45-4083-4224-a416-c8bdb4535470',
+    name: 'john',
+    lastName: 'doe',
+    phoneNumber: '+5584988888888',
+    email: 'john@mail.com',
+    password: '12345678a',
+    fullName: 'john doe',
+    refreshToken: 'token',
+    createdAt: '2022-06-06T20:44:57.107Z',
+    updatedAt: '2022-06-06T20:44:57.107Z'
+  },
+  {
+    id: '3ca37fd8-800c-473c-9a77-eb8b26d9e059',
+    name: 'ze',
+    lastName: 'ninguem',
+    phoneNumber: '+5584988888888',
+    email: 'ze@mail.com',
+    password: '87654321a',
+    fullName: 'ze ninguem',
+    refreshToken: 'token',
+    createdAt: '2022-06-06T20:44:58.107Z',
+    updatedAt: '2022-06-06T20:44:58.107Z'
+  }
+]
+
+const UserMock = DBConnectionMock.define('users', userDataMock, { autoQueryFallback: false })
+
+UserMock.$queryInterface.$useHandler((query:string, queryOptions: any, done: any) => {
+  if(query === 'findOne') {
+    
+    const search = queryOptions[0].where
+    const filters = Object.keys(search)
+    
+    if (filters.length === 1) {
+      const filter = filters.toString()
+      const user = userDataMock.find( (user: any) => user[`${filter}`] === search[`${filter}`]) 
+      return user ? user : null
+    }
+    // TODO more than 1 filter search
+  }
+})
+
+export = UserMock
