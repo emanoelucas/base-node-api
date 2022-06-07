4f1b74a04bfd1498461d57c878d2fd7185f28fe5
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-06-07 15:39:28 -0300

    load user by id test

---
+++ __tests__/unit/infra/repositories/load-user-by-id.test.ts
@@ -0,0 +1,25 @@
+const userMock = require('../../../../__mocks__/user-model')
+import { loadUserById } from '../../../../src/infra/repositories/users'
+import MissingParamError from '../../../../src/utils/erros/missing-param-error'
+
+jest.mock('./../../../../src/infra/database/models/user', () => {
+  return userMock
+})
+
+describe('Load user by id repository', () => {
+	it('should return a user if is found', async () => {
+    const mockId = '03c22d45-4083-4224-a416-c8bdb4535470'
+    const user = await loadUserById.load(mockId)
+    expect(user?.id).toBe(mockId)
+	})
+  it('should return null no user is found', async () => {
+    const mockId = 'wrong_id'
+    const user = await loadUserById.load(mockId)
+    expect(user).toBeNull()
+	})
+  it('should throw if no id is provided', async () => {
+    const mockId = ''
+    const promise = loadUserById.load(mockId)
+    expect(promise).rejects.toThrow(new MissingParamError('id'))
+	})
+})
