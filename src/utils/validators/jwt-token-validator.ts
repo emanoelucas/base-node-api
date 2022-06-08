490e206d565cf89a563eb1cb19e9f8122f3bd7be
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-06-08 11:39:50 -0300

    change validator functions to class

--- src/utils/validators/jwt-token-validator.ts
+++ src/utils/validators/jwt-token-validator.ts
@@ -1,17 +1,20 @@
 import jwt from 'jsonwebtoken'
 import MissingParamError from '../erros/missing-param-error'
 
-export default (token: string, secret: string) => {
+class JwtTokenValidator {
 
-  if (!token)
-    throw new MissingParamError('token')
-  if (!secret)
-    throw new MissingParamError('secret')
+  validate (token: string, secret: string) {
+    if (!token)
+      throw new MissingParamError('token')
+    if (!secret)
+      throw new MissingParamError('secret')
 
-  try {
-    jwt.verify(token, secret)
-    return true
-  } catch (err) {
-    return false
+    try {
+      jwt.verify(token, secret)
+      return true
+    } catch (err) {
+      return false
+    }
   }
 }
+export default new JwtTokenValidator()
--- src/utils/validators/request-body-validator.ts
+++ src/utils/validators/request-body-validator.ts
@@ -2,25 +2,29 @@ import InvalidParamError from "../erros/invalid-param-error"
 import MissingParamError from "../erros/missing-param-error"
 import { BadRequestError } from "../http/erros"
 
-export default (requiredParams: Array<string>, receivedData: any) => {
-  
-  if (!Array.isArray(requiredParams))
-    throw new InvalidParamError('requiredParams is not an array')
-  if (!requiredParams.length)
-    throw new MissingParamError('requiredParams')
-  if (!receivedData)
-    throw new MissingParamError('receivedData')
-  
+class RequestBodyValidator {
 
-  let missingParams: Array<string> = []
-  const receivedParams = Object.keys(receivedData)
+  validate (requiredParams: Array<string>, receivedData: any) {
+    if (!Array.isArray(requiredParams))
+      throw new InvalidParamError('requiredParams is not an array')
+    if (!requiredParams.length)
+      throw new MissingParamError('requiredParams')
+    if (!receivedData)
+      throw new MissingParamError('receivedData')
+    
 
-  requiredParams.forEach(param => {
-    if (!receivedParams.includes(param))
-      missingParams.push(param)
-  })
+    let missingParams: Array<string> = []
+    const receivedParams = Object.keys(receivedData)
 
-  if (!missingParams.length)
-    return 
-  throw new BadRequestError(`Missing fields: ${missingParams.join(', ')}.`)
+    requiredParams.forEach(param => {
+      if (!receivedParams.includes(param))
+        missingParams.push(param)
+    })
+
+    if (!missingParams.length)
+      return 
+    throw new BadRequestError(`Missing fields: ${missingParams.join(', ')}.`)
+  }
 }
+
+export default new RequestBodyValidator()
