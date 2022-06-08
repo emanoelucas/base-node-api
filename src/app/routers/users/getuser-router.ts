df9ae8fd2a762ff987272729b1619c336a24d738
Emanoel Lucas <emanoel.costa.ext@sodexo.com>
2022-06-08 14:36:12 -0300

    class routers

--- src/app/routers/users/getuser-router.ts
+++ src/app/routers/users/getuser-router.ts
@@ -3,16 +3,21 @@ import { Request, Response, NextFunction } from 'express'
 import retrieve from '../../cases/users/retrieve'
 import HttpResponse from '../../../utils/http/response/http-response'
 
-export default async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const id = req.user.sub
-    const user = await retrieve.run(id)
+class GetUserRouter{
+  get = async (req: Request, res: Response, next: NextFunction) => {
+    try {
 
-    res.send(
-      HttpResponse.success( { message: 'User retrieved', data: {user: user.retrievableData()} } )
-    )
-
-  } catch (error) {
-    next(error)
+      const id = req.user.sub
+      const user = await retrieve.run(id)
+  
+      res.send(
+        HttpResponse.success( { message: 'User retrieved', data: {user: user.retrievableData()} } )
+      )
+  
+    } catch (error) {
+      next(error)
+    }
   }
 }
+
+export default new GetUserRouter()
--- src/app/routers/users/signup-router.ts
+++ src/app/routers/users/signup-router.ts
@@ -4,20 +4,26 @@ import { requestBodyValidator }from '../../../utils/validators'
 import creation from '../../cases/users/creation'
 import HttpResponse from '../../../utils/http/response/http-response'
 
-export default async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const requiredParams = ['name', 'lastName', 'phoneNumber', 'email', 'password', 'repeatPassword']
-    const body = req.body
+class SignUpRouter {
+
+  sign = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+      const requiredParams = ['name', 'lastName', 'phoneNumber', 'email', 'password', 'repeatPassword']
+      const body = req.body
+    
+      requestBodyValidator.validate(requiredParams, body)
   
-    requestBodyValidator.validate(requiredParams, body)
-
-    const user = await creation.create(body.name, body.lastName, body.phoneNumber, body.email, body.password, body.repeatPassword)
-
-    res.send(
-      HttpResponse.success( { message: 'Account created', data: {user: user.retrievableData()} } )
-    )
-
-  } catch (error) {
-    next(error)
+      const user = await creation.create(body.name, body.lastName, body.phoneNumber, body.email, body.password, body.repeatPassword)
+  
+      res.send(
+        HttpResponse.success( { message: 'Account created', data: {user: user.retrievableData()} } )
+      )
+  
+    } catch (error) {
+      next(error)
+    }
   }
 }
+
+export default new SignUpRouter()
